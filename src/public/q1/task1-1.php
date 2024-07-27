3466c73edf609e3a299b7f85afd6428c83d02dae
atsuki-kitahori <pcp.m.t15@gmail.com>
2024-07-27 11:45:39 +0900

    クラス化をしてみよう 問題1

--- src/public/q1/task1-1.php
+++ src/public/q1/task1-1.php
@@ -1,17 +1,6 @@
 <?php
-$dbUserName = 'root';
-$dbPassword = 'password';
-$pdo = new PDO(
-    'mysql:host=mysql; dbname=tq_quest; charset=utf8',
-    $dbUserName,
-    $dbPassword
-);
+require_once __DIR__ . '/../../vendor/autoload.php';
+use App\Incomes;
 
-$sql = 'SELECT * FROM incomes';
-$statement = $pdo->prepare($sql);
-$statement->execute();
-$incomes = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-echo '<pre>';
-var_dump($incomes);
-echo '<pre>';
+$incomes = new Incomes();
+var_dump($incomes->fetchAllIncomes());
