d40564a971c7c2fbf8ccbbe97257f26042f1c053
atsuki-kitahori <pcp.m.t15@gmail.com>
2024-07-27 10:43:55 +0900

    問題1

---
+++ src/public/q1/task1-1.php
@@ -0,0 +1,17 @@
+<?php
+$dbUserName = 'root';
+$dbPassword = 'password';
+$pdo = new PDO(
+    'mysql:host=mysql; dbname=tq_quest; charset=utf8',
+    $dbUserName,
+    $dbPassword
+);
+
+$sql = 'SELECT * FROM incomes';
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$incomes = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<pre>';
+var_dump($incomes);
+echo '<pre>';
